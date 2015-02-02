41c7ebf45c6b0bfd61991f7cfc279b0d8e8a3704
Sanjiv Jha <sanjiv@joshsoftware.com>
2015-02-02 20:10:42 +0530

    added test case for sms gateway

--- test/test_sms_gateway_me.rb
+++ test/test_sms_gateway_me.rb
@@ -1,14 +1,18 @@
 require 'helper'
 require 'sms_gateway_me'
-require 'byebug'
 
 class TestSmsGatewayMe < Minitest::Test
+  
   def setup
     @config = SmsGatewayMe::Configuration.new
   end
 
   def test_configuration_object
     assert_instance_of SmsGatewayMe::Configuration, SmsGatewayMe.config 
+  end
+
+  def test_respond_to_setup
+    assert SmsGatewayMe.respond_to?(:setup), true
   end
 
 end
